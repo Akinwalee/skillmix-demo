5278044e824825002943d81cfbad438a3b4f8403
Akinwalee <Akinwalee>
2022-08-30 11:01:07 +0100

    Populate configuration and automation files

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            versionversion = "~> 4.0"
+        }
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+#create a remote backend
+terraform {
+  backend "s3" {
+    bucket = "automated-skillmix-bucket"
+    key = "path/to/my/key"
+  }
+}
